d9bd2507907f8ed96330536153b1ae4279033b37
rdeepak2002 <rdeepak2002@gmail.com>
2020-05-06 19:42:27 -0700

    created latency calculation

--- index.js
+++ index.js
@@ -6,25 +6,37 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 const FPS = 15
-const webcamCapture = new cv.VideoCapture(0)
+const faceClassifier = new cv.CascadeClassifier(path.join(__dirname, 'haarcascade_frontalface_default.xml'));
+let webcamCapture = new cv.VideoCapture(0)
 
 app.get('/', (req, res) =>  {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-setInterval(() => {
+function processVideo() {
+    const begin = Date.now();
+
     try {
-        let frame = webcamCapture.read()
-        frame = frame.resize(360, 640)
+        let frame = webcamCapture.read().resize(360, 640)
         const image = cv.imencode('.jpg', frame).toString('base64')
         io.emit('image', image)
-    }
-    catch(error) {
+        let delay = 1000 / FPS - (Date.now() - begin);
+    } catch (error) {
         console.error(error)
         webcamCapture.release()
+        webcamCapture.delete()
+        webcamCapture = new cv.VideoCapture(0)
         io.emit('error', error)
     }
 
-}, 1000/FPS)
+    let latency = (Date.now() - begin)
+    let delay = 1000 / FPS - latency
+
+    io.emit('latency', latency)
+
+    setTimeout(processVideo, delay)
+}
+
+setTimeout(processVideo, 0)
 
 server.listen(3000)
